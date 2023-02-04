abf9ffb729f28bcd8563d1b674ba20f0b9b4fe58
Rudi Benkovic <rudi.benkovic@gmail.com>
2023-02-04 16:06:52 +0100

    something

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, any::Any};
+use std::{collections::HashMap, any::{Any, TypeId}, sync::Arc};
 
 use petgraph::{Graph, algo::{toposort, tred::dag_to_toposorted_adjacency_list}, adj::NodeIndex, visit::IntoNeighbors};
 use uuid::Uuid;
@@ -10,12 +10,36 @@ use uuid::Uuid;
 pub struct Node {
     pub id: Uuid,
     pub inputs: Vec<Source>,
-    pub outputs: Vec<Source>
+    pub outputs: Vec<Source>,
+    pub func: NodeFunc
+}
+
+#[derive(Clone)]
+pub struct NodeFunc {
+    pub f: Arc<Box<dyn Fn(&Context) -> Container>>
+}
+
+impl std::fmt::Debug for NodeFunc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("NodeFunc").field("f", &"some func").finish()
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub enum Source {
     Id(String)
+}
+
+impl Source {
+    pub fn get_id(&self) -> String {
+        match self {
+            Source::Id(id) => id.clone(),
+        }
+    }
+}
+
+pub fn id(s: impl Into<String>) -> Source {
+    Source::Id(s.into())
 }
 
 
@@ -42,9 +66,10 @@ impl Pipeline {
         for n in nodes {
             let dst = graph_nodes.get(&n.id).expect("Dest node not found");
             for i in &n.inputs {
-                let node_source_id = outputs.get(i).expect("No source node found");
-                let src = graph_nodes.get(node_source_id).expect("Source node not found");
-                g.add_edge(src.clone(), dst.clone(), i.clone());
+                if let Some(node_source_id) = outputs.get(i) {
+                    let src = graph_nodes.get(node_source_id).expect("Source node not found");
+                    g.add_edge(src.clone(), dst.clone(), i.clone());
+                }
             }
         }
 
@@ -61,30 +86,118 @@ impl Pipeline {
         
         
     }
+
+    pub fn run(&self, mut container: Container) -> Container {
+        for n in &self.nodes {
+
+            // todo: copy just the needed inputs?
+            let ctx = Context {
+                inputs: container
+            };
+            let mut res = (n.func.f)(&ctx);
+            
+            container = ctx.inputs;
+            for o in &n.outputs {
+                container.data.insert(o.get_id(), res.data.remove(&o.get_id()).expect("Missing output from node func"));
+            }
+        }
+
+        container
+    }
 }
 
+
+pub struct Context {
+    pub inputs: Container
+}
+
+impl Context {
+    
+}
 
 pub struct Container {
-    pub data: HashMap<String, Box<dyn Any>>
+    data: HashMap<String, Box<dyn Any>>
 }
 
+impl Container {
+    pub fn new() -> Self {
+        Container {
+            data: HashMap::default()
+        }
+    }
 
+    pub fn insert<V>(&mut self, key: &str, value: V)
+        where V: 'static 
+    {
+        if self.data.contains_key(key) {
+            panic!("already exists");
+        }
+
+        self.upsert(key, value)
+    }
+
+    pub fn upsert<V>(&mut self, key: &str, value: V)
+        where V: 'static 
+    {
+        self.data.insert(key.to_string(), Box::new(value));
+    }
+
+    pub fn get<V>(&self, key: &str) -> &V
+        where V: 'static
+    {
+        let v = self.data.get(key).expect("id not found");
+        
+        if let Some(v) = v.downcast_ref() {
+            v
+        } else {
+            let msg = format!("Requested type {:?}, stored type is {:?}", TypeId::of::<V>(), v.type_id());
+            panic!("{}", msg);
+        }        
+    }
+}
+
+pub enum QupidoError {
+
+}
+
+pub type QupidoResult<T = ()> = Result<T, QupidoError>;
 
 
 #[test]
 fn test_nodes_topo() {
     let a = Node {
         id: Uuid::new_v4(),
-        inputs: vec![],
-        outputs: vec![Source::Id("foo".into())]
+        inputs: vec![id("a"), id("b")],
+        outputs: vec![id("foo")],
+        func: NodeFunc { f: 
+            Arc::new(Box::new(
+                |c| {
+                    let a: &u32 = c.inputs.get("a");
+                    let b: &u32 = c.inputs.get("b");
+
+                    let res = a + b;
+
+                    let mut r = Container::new();
+                    r.insert("foo", res);
+                    r
+                }
+            ))
+        }
     };
 
     let b = Node {
         id: Uuid::new_v4(),
-        inputs: vec![Source::Id("foo".into())],
-        outputs: vec![Source::Id("zzz".into())]
+        inputs: vec![id("foo")],
+        outputs: vec![],
+        func: NodeFunc { f: Arc::new(Box::new(|c| {
+            let v = c.inputs.get::<u32>("foo");
+
+            println!("result={}", v);
+            Container::new()
+        })) }
     };
 
+    /*
     let c = Node {
         id: Uuid::new_v4(),
         inputs: vec![Source::Id("foo".into())],
@@ -108,11 +221,22 @@ fn test_nodes_topo() {
         inputs: vec![],
         outputs: vec![Source::Id("456".into())]
     };
+    */
     
 
-    let pipeline = Pipeline::from_nodes(&[a, b, c, d, e, f]);
+    //let pipeline = Pipeline::from_nodes(&[a, b, c, d, e, f]);
 
+    let container = {
+        let mut c = Container::new();
+        c.insert("a", 2 as u32);
+        c.insert("b", 2 as u32);
+        c
+    };
 
+    let pipeline = Pipeline::from_nodes(&[a, b]);
+    let result = pipeline.run(container);
+
+        
     
 
 
